f3e657e16456bb84ed0ca2a65efcac998a8a3a3a
kalinkesilvio <silvio.kalinke60@gmail.com>
2023-11-28 19:40:33 +0100

    init Entities

--- src/main/java/at/htl/entity/Company.java
+++ src/main/java/at/htl/entity/Company.java
@@ -1,4 +1,41 @@
 package at.htl.entity;
 
-public class Company {
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Company extends PanacheEntity {
+
+    public String email;
+    public String password;
+    public String companyName;
+    public String websiteUrl;
+    public String branche;
+    public String imageUrl;
+
+    @JoinColumn
+    @ManyToOne(cascade = CascadeType.ALL)
+    public Address address;
+
+    public Company() {
+    }
+
+    public Company(String email, String password, String companyName) {
+        this.email = email;
+        this.password = password;
+        this.companyName = companyName;
+    }
+
+    public Company(String email, String password, String companyName, String websiteUrl, String branche, Address address, String imageUrl) {
+        this.email = email;
+        this.password = password;
+        this.companyName = companyName;
+        this.websiteUrl = websiteUrl;
+        this.branche = branche;
+        this.address = address;
+        this.imageUrl = imageUrl;
+    }
 }
