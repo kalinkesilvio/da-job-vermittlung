2802cd4b2aa0d2f819ed386fb439c605c6eda952
kalinkesilvio <silvio.kalinke60@gmail.com>
2023-12-03 14:40:38 +0100

    basic Methods for db interactions

--- src/main/java/at/htl/controller/CompanyRepository.java
+++ src/main/java/at/htl/controller/CompanyRepository.java
@@ -1,6 +1,7 @@
 package at.htl.controller;
 
 import at.htl.entity.Applicant;
+import at.htl.entity.Company;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
@@ -8,17 +9,17 @@ import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
 
 @ApplicationScoped
-public class ApplicantRepository implements PanacheRepository<Applicant> {
+public class CompanyRepository implements PanacheRepository<Company> {
 
     @Inject
     EntityManager em;
 
     @Transactional
-    public void save(Applicant applicant) {
-        em.persist(applicant);
+    public void save(Company company) {
+        em.persist(company);
     }
 
-    public Applicant getApplicantById(Long id) {
+    public Company getCompanyById(Long id) {
         return findById(id);
     }
 
@@ -26,5 +27,4 @@ public class ApplicantRepository implements PanacheRepository<Applicant> {
     public void remove(Long id) {
         deleteById(id);
     }
-
 }
