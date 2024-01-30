271e7037207e7af40ec91dae9acc2b3f0d88c7ec
kalinkesilvio <silvio.kalinke60@gmail.com>
2024-01-30 18:46:29 +0100

    JobOfferRepositoryTest getJobOffersWithPartialString() Test implemented and working

---
+++ src/test/java/at/htl/controller/JobOfferRepositoryTest.java
@@ -0,0 +1,34 @@
+package at.htl.controller;
+
+import at.htl.entity.JobOffer;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
+class JobOfferRepositoryTest {
+
+    @Inject
+    JobOfferRepository jobOfferRepository;
+
+    @Test
+    void getJobOffersWithPartialString() {
+        String partial = "Gastr";
+
+        List<JobOffer> jobOffers = jobOfferRepository.getJobOffersWithPartialString(partial);
+
+        assertFalse(jobOffers.isEmpty());
+        assertThat(jobOffers.size(), is(2));
+        assertEquals("Gastronomie", jobOffers.get(0).category);
+        assertEquals("Gastronomie", jobOffers.get(1).category);
+        assertEquals("Koch an Wochenenden", jobOffers.get(0).title);
+        assertEquals("Kellner an Wochenenden", jobOffers.get(1).title);
+    }
+}
