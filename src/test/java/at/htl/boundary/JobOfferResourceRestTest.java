79b8f42a3d59546ab823799c65f9ed2fa44a7fa5
kalinkesilvio <silvio.kalinke60@gmail.com>
2024-01-31 12:37:51 +0100

    JobOfferResourceRestTest getById() Test implemented

---
+++ src/test/java/at/htl/boundary/JobOfferResourceRestTest.java
@@ -0,0 +1,78 @@
+package at.htl.boundary;
+
+import at.htl.entity.Company;
+import at.htl.entity.JobOffer;
+import io.quarkus.test.TestTransaction;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+@TestTransaction
+@TestHTTPEndpoint(JobOfferResource.class)
+class JobOfferResourceRestTest {
+
+    @Inject
+    CompanyResource companyResource;
+    private JobOffer jobOffer1;
+    private Company testCompany;
+
+    @BeforeEach
+    void setUp() {
+        testCompany = new Company();
+        testCompany.id = 30L;
+        testCompany.companyName = "Kuckuruz";
+        testCompany.branche = "Gastwirtschaft";
+        testCompany.email = "kukcuruz@office.gmail.com";
+
+        jobOffer1 = new JobOffer();
+        jobOffer1.id = 1L;
+        jobOffer1.category = "Solartechnik";
+        jobOffer1.salary = 3500.00;
+        jobOffer1.condition = "Bachelor Solartechnik & Energieerhaltung";
+        jobOffer1.title = "End-Testing für Solartechnik";
+    }
+
+    @Test
+    void create() {
+
+        this.jobOffer1.company = (Company) companyResource.getById(1L).getEntity();
+
+        given()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(this.jobOffer1)
+                .when()
+                .post("/create")
+                .then()
+                .statusCode(Response.Status.CREATED.getStatusCode())
+                .header("location", "http://localhost:9090/api/joboffer/1");
+    }
+
+    @Test
+    void getById() {
+        given()
+                .pathParam("id", 13)
+                .when()
+                .get("/{id}")
+                .then()
+                .statusCode(Response.Status.OK.getStatusCode())
+                .body("id", is(13),
+                        "jobbranche", is("Kartografie"),
+                        "salary", is(3000));
+    }
+
+    @Test
+    void getAll() {
+    }
+
+    @Test
+    void getByStringPartial() {
+    }
+}
