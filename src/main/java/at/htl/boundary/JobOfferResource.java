1fda606c8fd65135184fe845e5c35f76b302ba5f
kalinkesilvio <silvio.kalinke60@gmail.com>
2024-02-07 11:10:42 +0100

    delete JobOffer Resource Endpoint implemented

--- src/main/java/at/htl/boundary/JobOfferResource.java
+++ src/main/java/at/htl/boundary/JobOfferResource.java
@@ -70,4 +70,14 @@ public class JobOfferResource {
     public Response update(JobOffer jobOffer) {
         return Response.ok(jobOfferRepository.getEntityManager().merge(jobOffer)).build();
     }
+
+    @DELETE
+    @Path("{id}")
+    public Response delete(@PathParam("id") Long id) {
+        boolean deleted = jobOfferRepository.deleteById(id);
+        if (deleted) {
+            return Response.ok().build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
 }
